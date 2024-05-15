846a0f0459d56f3932dbb9fa09c17ec14e792324
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-05-15 13:51:15 +0200

    delte query example

--- app.js
+++ app.js
@@ -22,21 +22,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', async (req, res) => {
     //res.send("hello World");//serves index.html
     const pageTitle = "Dynamic webpage";
-    const sql = 'SELECT * FROM students';
+    const sql = 'SHOW tables';
     const dbData = await db.query(sql);
     console.log(dbData);
     res.render('index', {pageTitle, dbData} );
 });
 
+let currentTable;
 app.post('/', async (req, res) => {
     //res.send("hello World");//serves index.html
     //getting input data from the form
     console.log(req.body);
     const tableName = req.body;
-
     const pageTitle = "Dynamic webpage";
-
     const sql = `SELECT * FROM ${tableName.table_name}`;
+    currentTable = tableName.table_name
     const dbData = await db.query(sql);
     console.log(dbData);
     const sql2 = `DESCRIBE ${tableName.table_name}`;
@@ -45,6 +45,34 @@ app.post('/', async (req, res) => {
     res.render('index', {pageTitle, dbData, dbDataHeaders} );
 });
 
+app.get('/removeData', async (req, res) => {
+    //res.send("hello World");//serves index.html
+    const pageTitle = "Dynamic webpage";
+    const sql = `SELECT * FROM ${currentTable}`;
+    const dbData = await db.query(sql);
+    console.log(dbData);
+    res.render('removeData', {pageTitle, dbData} );
+});
+app.post('/removeData', async (req, res) => {
+    //res.send("hello World");//serves index.html
+    //getting input data from the form
+    console.log(req.body);
+    const requestData = req.body;
+    const pageTitle = "Dynamic webpage";
+    //execute delete query on a table.row
+    const sqlDeleteQuery = `DELETE FROM ${currentTable} WHERE id=${requestData.id}`;
+    const deleteQuery = await db.query(sqlDeleteQuery);
+    console.log(deleteQuery);
+    //get table data
+    const sql = `SELECT * FROM ${currentTable}`;
+    const dbData = await db.query(sql);
+    //get table headers
+    const sql2 = `DESCRIBE ${currentTable}`;
+    const dbDataHeaders = await db.query(sql2);
+    console.log(dbDataHeaders);
+    //show webpage to the user
+    res.render('removeData', {pageTitle, dbData, dbDataHeaders} );
+});
 
 
 
